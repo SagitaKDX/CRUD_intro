3105eab3aff7759e193ab057c14506e334baaa33
SagitaKDX <leminhl1412@gmail.com>
2025-07-04 10:20:25 +0700

    update part 3

---
+++ server/api/controllers/vocabController.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Vocab = mongoose.model('Vocab');
+
+exports.list_all_words = (req , res) => {
+    Vocab.find({}, (err, vocab) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json(words);
+    });
+};
+
+
+exports.create_a_word = (req, res) => {
+    const new_word = new Vocab(req.body);
+    new_word.save((err, word) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.status(201).json(word);
+    });
+};
+
+exports.read_a_word = (req , res) =>{
+    vocab.findById(req.params.wordId, (err, word) => {
+    req.body,
+    {new : true},
+    (err, word) => {
+        if (err) {
+            return res.status(500).send(err);
+        res.json
+
+}
